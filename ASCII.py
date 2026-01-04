6464d619ecc2a294b88712d2ebdf2fa9baf9a248
giandoctor <gian.doctor@hotmail.com>
2026-01-05 09:26:22 +1100

    ASCII

---
+++ ASCII.py
@@ -0,0 +1,8 @@
+#First Program
+lower=str(input("Enter anything you want:"))
+uppercase=lower.upper()
+print(uppercase)
+#Second Program
+char =str(input("Enter a letter:"))
+ascii_value = ord(char)
+print("The ASCII value of ",(char),"is",(ascii_value))
